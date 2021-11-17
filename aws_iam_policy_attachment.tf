e8a35f176cc2b9fdfe23457b3eebae6d7fc1f2b1
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-11-18 00:15:54 +0900

    fix name

--- aws_iam_policy_attachment.tf
+++ aws_iam_policy_attachment.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy_attachment" "github_actions_readonly_policy" {
-  name = "github-actions-readonly"
+  name = "${var.resource_prefix}-github-actions-readonly"
   roles = [
     aws_iam_role.github_actions_readonly_role.name,
   ]
