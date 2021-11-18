cb0f29c8e534ff8565d03d657a780298918d2711
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-11-19 00:20:10 +0900

    add workflow

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "cluster_name" {
   description = "Cluster name of EKS."
   value       = var.resource_prefix
 }
+
+output "role_to_assume" {
+  description = "IAM Role to assume."
+  value       = aws_iam_role.github_actions_role.arn
+}
