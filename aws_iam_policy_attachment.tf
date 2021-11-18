604f1e4541b35b988ef2430dab8e7a3bf64cceab
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-11-18 23:11:51 +0900

    fix validate

--- aws_iam_policy_attachment.tf
+++ aws_iam_policy_attachment.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_policy_attachment" "github_actions_readonly_policy" {
   name = "${var.resource_prefix}-github-actions-readonly"
   roles = [
-    aws_iam_role.github_actions_readonly_role.name,
+    aws_iam_role.github_actions_role.name,
   ]
 
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
